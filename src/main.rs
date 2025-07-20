17ed82f190f74a6008e0ea139037ab10bce2ec29
vasie <59412971+vasie1337@users.noreply.github.com>
2025-07-20 16:29:29 +0200

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use tracing::info;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_target(false)
+        .with_file(true)
+        .with_line_number(true)
+        .without_time()
+        .init();
+
+    info!("Hello, world!");
 }
